0caf6327354c1eed34e580b9f80d78732a84665b
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2023-09-14 15:33:57 +0900

    Initial commit

---
+++ src/main/java/org/taerock/apitest01/ApiTest01Application.java
@@ -0,0 +1,13 @@
+package org.taerock.apitest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTest01Application.class, args);
+    }
+
+}
