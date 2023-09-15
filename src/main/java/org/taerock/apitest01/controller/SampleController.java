4b7073c45538cb383803a029e4c1d472925527f9
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2023-09-15 09:50:50 +0900

    project setting for api token test

--- src/main/java/org/taerock/apitest01/controller/SampleController.java
+++ src/main/java/org/taerock/apitest01/controller/SampleController.java
@@ -1,5 +1,6 @@
 package org.taerock.apitest01.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequestMapping("/api/sample")
 public class SampleController {
 
+    @Operation(summary = "SampleTest", description = "GET 방식 화면 테스트")
     @GetMapping("/doA")
     public List<String> doA(){
         return Arrays.asList("AAA", "BBB", "CCC");
